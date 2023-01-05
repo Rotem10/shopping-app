ea8081ae00a51df3350c3824891e33462693b881
Rotem10 <65648972+Rotem10@users.noreply.github.com>
2023-01-05 13:58:54 +0200

    ICacheAlgo interface

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,1 +1,13 @@
 console.log('hello');
+
+interface ICacheAlgo<K, V> {
+  getElement(key: K): V | undefined;
+  setElement(key: K, value: V): K | undefined;
+  removeElement(key: K): boolean;
+}
+
+// abstract class AbstractCacheAlgo {
+//     constructor(parameters) {
+
+//     }
+// }
